1bcec98b6571e5f27702b9daf082449e6515b5de
suimingzhe <1025935864@qq.com>
2025-02-27 15:13:18 +0800

    Update channel gate mechanism

--- models/EdgeConv.py
+++ models/EdgeConv.py
@@ -3,6 +3,17 @@ from torch.nn import Module, Linear, ModuleList
 from .utils import *
 from .feature import *
 
+class ChannelGate(Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.gate = Linear(channels, channels) # (60, 60)
+    def forward(self, x):
+        # 全局平均池化 (B, N, C)
+        global_feat = x.mean(dim=2)
+        # 动态生成门控权重 (B, N, C)
+        gate_weights = torch.sigmoid(self.gate(global_feat).transpose(1,2)).transpose(1,2)
+        # 应用门控权重
+        return x * gate_weights.unsqueeze(2)  # 扩展为 (B, N, K, C)
 
 class OffsetPredictor(Module):
     """预测每个点的位置偏移量"""
@@ -18,7 +29,6 @@ class OffsetPredictor(Module):
     def forward(self, x):
         return self.mlp(x) * 0.1    # 缩放偏移量，防止过大扰动
     
-
 
 class DenseEdgeConv(Module):
 
@@ -41,18 +51,17 @@ class DenseEdgeConv(Module):
 
         if self.use_deformable:
             self.edge_attention = nn.Sequential(
-                Linear(in_channels, 12),
+                Linear(in_channels, 12), # (24, 12)
                 nn.ReLU(),
                 Linear(12, 16)
             )
 
-
+        self.channel_gate1 = ChannelGate(in_channels + growth_rate)
+        self.channel_gate2 = ChannelGate(in_channels + 2 * growth_rate)
+        self.channel_gate3 = ChannelGate(in_channels + 3 * growth_rate)
         self.layer_first = FCLayer_first(3*in_channels, 4*in_channels, growth_rate, bias=True)
-
         self.layer_last = FCLayer(in_channels + (num_fc_layers - 1) * growth_rate, growth_rate, bias=True, activation=None)
-
         self.layers = ModuleList()
-
         for i in range(1, num_fc_layers-1):
             self.layers.append(FCLayer(in_channels + i * growth_rate, growth_rate, bias=True, activation=activation))
 
@@ -100,6 +109,9 @@ class DenseEdgeConv(Module):
             x.unsqueeze(-2).repeat(1, 1, self.knn, 1) # (B, N, K, d)
         ], dim=-1)  # (B, N, K, d+c)
 
+        # print(y.shape)
+        # y = self.channel_gate1(y)
+
         # Intermediate Layers
         for layer in self.layers:
             y = torch.cat([
@@ -107,12 +119,14 @@ class DenseEdgeConv(Module):
                 y,                  # (B, N, K, c+d)
             ], dim=-1)  # (B, N, K, d+c+...)
         
+        y = self.channel_gate2(y)
         # Last Layer
         y = torch.cat([
             self.layer_last(y), # (B, N, K, c)
             y                   # (B, N, K, d+(L-1)*c)
         ], dim=-1)  # (B, N, K, d+L*c)
 
+        # y = self.channel_gate3(y)
         # Pooling
         y = self.aggr(y, dim=-2)
 
